952cbb5baecbbb2261dce3fcdc6378255d3bc77e
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-01-20 11:07:02 +0100

    Error handling

--- script.js
+++ script.js
@@ -4,12 +4,19 @@
     const URL_BASE = 'https://www.swapi.tech/api/people/?name=';
     const tareaResult = document.querySelector('#tareaResult');
     const tareaLabelFor = tareaResult.nextElementSibling;
+    const divResultArea = tareaLabelFor.parentElement;
 
 
 
     api.get = function (e) {
         e.preventDefault();
         /*Skriv din kod här*/
+        tareaLabelFor.innerHTML = 'Loading';
+        divResultArea.classList.remove('text-danger');
+        divResultArea.classList.remove('text-success');
+        divResultArea.classList.add('text-info');
+
+
         const searchText = txtSearchInput.value;
         // const searchText='chewbacca'; //test
 
@@ -19,15 +26,19 @@
             .then(result => result.json())
             .then(data => {
                 /*Och här*/
+                
                 if (data.result.length > 0) {
                     tareaResult.disabled = false;
+                    divResultArea.classList.remove('text-info');
+                    divResultArea.classList.add('text-success');
                     tareaLabelFor.innerHTML = 'Karaktärer';
                 }
                 else {
                     tareaResult.disabled = true;
-                    tareaLabelFor.innerHTML = 'Resultat';
+                    tareaLabelFor.innerHTML = 'Ej Resultat';
                 }
 
+                tareaResult.innerHTML = '';
                 for (const character of data.result) {
                     const resultString =
                         `Name: ${character.properties.name}, Height: ${character.properties.height},Mass: ${character.properties.mass}, Gender: ${character.properties.gender}, Hair color: ${character.properties.hair_color}\n`;
@@ -35,7 +46,13 @@
                     tareaResult.innerHTML += resultString;
                 }
             })
-            .catch(err => console.log(err))
+            .catch((err) => {
+                console.log(err);
+                divResultArea.classList.remove('text-info');
+                divResultArea.classList.add('text-danger');
+                tareaLabelFor.innerHTML = 'Error';
+                tareaResult.innerHTML = err;
+            })
     }
 })(window.api = window.api || {});
 // const btnSearch = document.querySelector('#btnSearch'); // no validation
